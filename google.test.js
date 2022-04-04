a4b3f5346cbc0a13453f46ce0a1bb800fdf127de
Nick Galvez <nicholas.galvez@gmail.com>
2022-04-04 11:12:47 -0700

    Update gitignore and add snapshots dir. Also modified the test to make it fail

--- google.test.js
+++ google.test.js
@@ -3,12 +3,15 @@ const { toMatchImageSnapshot } = require('jest-image-snapshot');
 
   expect.extend({ toMatchImageSnapshot });
 
+/**
+ * This test should fail. The screenshot in the test folder uses "Hello" as a test string in the search bar
+ */
 it('renders correctly', async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setViewport({width: 1920, height: 1080})
   await page.goto('https://www.google.com', { waitUntil: 'networkidle0' })
-  await page.type('input[title="Search"]', 'Hello', { delay: 100 })
+  await page.type('input[title="Search"]', 'Goodbye', { delay: 100 })
   const image = await page.screenshot();
 
   expect(image).toMatchImageSnapshot();
